539f7729fdccae1b5d741463c98b12ab27068ba6
PriyanshuValiya <147643182+PriyanshuValiya@users.noreply.github.com>
2026-05-03 18:02:29 +0530

    Update Footer.tsx

--- www/components/Footer.tsx
+++ www/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 bg-black text-white">
       <div className="border-t border-gray-800 mt-8 pt-8 flex flex-col md:flex-row justify-between items-center">
-        <Link href="https://portfolio.priyanshuvaliya.me"
+        <Link href="https://priyanshuvaliya.dev"
             target="_blank">
         <p className="text-gray-400">Priyanshu Valiya © 2024 </p></Link>
         <div className="flex space-x-6 mt-4 md:mt-0">
@@ -17,7 +17,7 @@ function Footer() {
             GitHub
           </Link>
           <Link
-            href="https://www.linkedin.com/in/priyanshu-valiya19012006"
+            href="https://www.linkedin.com/in/priyanshu-valiya"
             target="_blank"
             className="text-gray-400 hover:text-white transition-colors"
           >
